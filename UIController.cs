cb03e30f161a5976403095b66e3ab191b9c0ac05
Baniface <den-m-v@mail.ru>
2018-08-04 22:45:22 +0300

    first for gh

---
+++ UIController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+	[SerializeField] private Text scoreLabel;
+	[SerializeField] private SettingsPopup settingsPopup;
+	private int _score;
+
+	void Awake() {
+		Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+	}
+
+	void OnDestroy() {
+		Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+	}
+
+	void Start() {
+		_score = 0;
+		UpdateScoreText();
+		settingsPopup.Close();
+	}
+
+	private void OnEnemyHit() {
+		_score += 1;
+		UpdateScoreText();
+	}
+
+	public void OnOpenSettings() {
+		settingsPopup.Open();
+	}
+
+	public void OnPointerDown() {
+		Debug.Log("pointer down");
+	}
+
+	private void UpdateScoreText() {
+		scoreLabel.text = _score.ToString();
+	}
+}
